0cfa3cef8b2e118cb903bd4fe94700f5fd96dda9
Eunike Pardede <eunikehedriani@gmail.com>
2024-03-18 22:01:04 +0100

    change name of game and add button

--- script.js
+++ script.js
@@ -1,67 +1,85 @@
-let level = document.querySelector('#level');
-let numOfLevel = Number(level.innerText);
+let score = document.querySelector('#score');
+let numOfScore = Number(score.innerText);
+fruitImg = [1,2,3,4,5,6]
 
 
-let numOfButterflies = 5;
+let numOfFruits = 5;
 const theLeftSide = document.querySelector('#leftSide');
 const theRightSide = document.querySelector('#rightSide');
 
-// create random butterflies on the left side
-function generateButterflies() {
-    for (let i = 0; i < numOfButterflies; i++) {
-        const butterfly = document.createElement('img');
-        butterfly.src = 'img/butterfly.png';
+// click start button to run the function
+document.querySelector('#startBtn').addEventListener('click', generateFruit);
+
+// create random fruits on the left side
+function generateFruit() {
+    for (let i = 0; i < numOfFruits; i++) {
+        const fruit = document.createElement('img');
+        const randomImg = Math.floor(Math.random()* fruitImg.length)
+        fruit.src = `img/${randomImg}.png`;
 
         const randomTop = Math.floor(Math.random() * 400) + 1;
-        const randomLeft = Math.floor(Math.random() * 400) + 1;
+        const randomLeft = Math.floor(Math.random() * 600) + 1;
 
-        butterfly.style.top = randomTop + 'px';
-        butterfly.style.left = randomLeft + 'px';
+        fruit.style.top = randomTop + 'px';
+        fruit.style.left = randomLeft + 'px';
 
-        theLeftSide.appendChild(butterfly);
+        theLeftSide.appendChild(fruit);
     }
 
     const leftSideImages = theLeftSide.cloneNode(true);
     leftSideImages.removeChild(leftSideImages.lastChild);
     theRightSide.appendChild(leftSideImages);
 
-    theLeftSide.lastChild.addEventListener('click', nextLevel);
+    theLeftSide.lastChild.addEventListener('click', nextScore);
     theLeftSide.addEventListener('click', gameOver);
 }
 
-// load the function
-window.addEventListener('load', generateButterflies);
 
-function nextLevel(event) {
+function nextScore(event) {
     event.stopPropagation(); //prevents further propagation of the current event in the capturing and bubbling phases.
-    numOfLevel += 1;
-    level.innerText = numOfLevel; // add level
-    numOfButterflies += 5;
-    while (theLeftSide.firstChild) { //all butterflies must be removed before a new set of faces is generated and added to the page. 
+    numOfScore += 100;
+    score.innerText = numOfScore; // add score
+    numOfFruits += 3;
+    while (theLeftSide.firstChild) { //all fruits must be removed before a new set of faces is generated and added to the page. 
         theLeftSide.removeChild(theLeftSide.firstChild);
     }
     while (theRightSide.firstChild) {
         theRightSide.removeChild(theRightSide.firstChild);
     }
-    generateButterflies();
+    generateFruit();
 
 }
 
-function gameOver() {
+//reset Button 
+document.querySelector('#resetBtn').addEventListener('click', resetGame);
+
+function resetGame(event){
+    event.stopPropagation(); 
+    score.innerText = 0;
+    theLeftSide.removeEventListener('click', gameOver);
+    theLeftSide.lastChild.removeEventListener('click', nextScore);
+    while (theLeftSide.firstChild) {
+        theLeftSide.removeChild(theLeftSide.firstChild);
+      } 
+    while (theRightSide.firstChild) {
+    theRightSide.removeChild(theRightSide.firstChild);
+    } 
+}
+
+
+// game over
+
+function gameOver(event) {
+    event.stopPropagation(); 
     alert('Game Over!');
     theLeftSide.removeEventListener('click', gameOver);
-    theLeftSide.lastChild.removeEventListener('click', nextLevel);
+    theLeftSide.lastChild.removeEventListener('click', nextScore);
+    while (theLeftSide.firstChild) {
+        theLeftSide.removeChild(theLeftSide.firstChild);
+      } 
+    while (theRightSide.firstChild) {
+    theRightSide.removeChild(theRightSide.firstChild);
+    } 
+    score.innerText = 0;
 }
 
-// const resetButton = document.getElementById('resetBtn');
-// resetButton.addEventListener('click', function () {
-//     document.getElementById('level').innerText = '1';
-
-//     while (theLeftSide.firstChild) { //all butterflies must be removed before a new set of faces is generated and added to the page. 
-//         theLeftSide.removeChild(theLeftSide.firstChild);
-//     }
-//     while (theRightSide.firstChild) {
-//         theRightSide.removeChild(theRightSide.firstChild);
-//     }
-//     generateButterflies();
-// });
